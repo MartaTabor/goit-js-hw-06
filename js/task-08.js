cf8f08632ede1e56003417bcbde67a4fdecad93b
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-18 22:38:11 +0000

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const regForm = event.target;
+  const email = regForm.elements.email.value;
+  const password = regForm.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const formObj = {
+      email: email,
+      password: password,
+    };
+    console.log(formObj);
+    form.reset();
+  }
+});
