26a26db8ce2668f9c2cb253ae277b0f7182dd864
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-19 19:26:19 +0000

    update

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,6 @@ const inputField = document.querySelector("#name-input");
 const outputField = document.querySelector("#name-output");
 
 inputField.addEventListener("input", (event) => {
-  const inputValue = event.currentTarget.value;
+  const inputValue = event.currentTarget.value.trim();
   outputField.textContent = inputValue !== "" ? inputValue : "Anonymous";
 });
