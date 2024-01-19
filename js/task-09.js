799bcf10e2f95fdebebed8c5699dc436a495312f
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-19 19:38:33 +0000

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const colorBody = document.querySelector("body");
+
+colorButton.addEventListener("click", () => {
+  colorBody.style.backgroundColor = getRandomHexColor();
+  colorSpan.innerHTML = getRandomHexColor();
+});
