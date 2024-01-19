865cfb1f3f84cf48d4f6ec098c414a3ad3b89c93
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-19 21:18:02 +0000

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document
+  .querySelector("button[data-create]")
+  .addEventListener("click", createBoxes);
+document
+  .querySelector("button[data-destroy]")
+  .addEventListener("click", destroyBoxes);
+
+const inputField = document.querySelector("input");
+const boxesField = document.querySelector("#boxes");
+
+function createBoxes(amonunt) {
+  const inputValue = inputField.value;
+
+  for (let i = 0; i < inputValue; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesField.append(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesField.innerHTML = "";
+  inputField.value = "";
+}
